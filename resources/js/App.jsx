032e4dbd4f77140461d8d6b98a1a9362c21ff5bc
David Delcid <franando14@gmail.com>
2021-11-13 15:35:43 -0600

    ruta corregida

--- resources/js/App.jsx
+++ resources/js/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-d
 import AccessibleNavigationAnnouncer from './components/AccessibleNavigationAnnouncer'
 
 const Layout = lazy(() => import('./containers/Layout'))
-const Login = lazy(() => import('./pages/Login'))
+const Login = lazy(() => import('./views/auth/Login'))
 const CreateAccount = lazy(() => import('./pages/CreateAccount'))
 const ForgotPassword = lazy(() => import('./pages/ForgotPassword'))
 

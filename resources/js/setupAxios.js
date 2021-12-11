5f7da5d5e88bf6ec70852ed7f10faa6f75056d88
David Delcid <franando14@gmail.com>
2021-12-11 07:26:04 -0600

    logoutSession

--- resources/js/setupAxios.js
+++ resources/js/setupAxios.js
@@ -1,5 +1,5 @@
 
-import { Logout } from "../services/authService"
+
 
 const setupAxios = (axios) => {
   axios.interceptors.request.use(
@@ -13,9 +13,11 @@ const setupAxios = (axios) => {
     return response
   }, error => {
     if (error.response.status == 401) {
-      Logout().then(() => {
-        localStorage.removeItem('user')
-      })
+      axios.post('api/logout')
+        .then(() => {
+          localStorage.removeItem('user')
+          history.push("/login")
+        })
     }
 
     return Promise.reject(error)
